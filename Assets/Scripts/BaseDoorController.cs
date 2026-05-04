13d1593be498b7927af08eaa3b4181ed32d75556
Danilepez <danilepez5@gmail.com>
2026-05-04 08:51:33 -0400

    fix: all the doors are opened

--- Assets/Scripts/BaseDoorController.cs
+++ Assets/Scripts/BaseDoorController.cs
@@ -40,7 +40,7 @@ public abstract class BaseDoorController : MonoBehaviour
         {
             bool hitThisDoor = hit.collider.GetComponentInParent<BaseDoorController>() == this;
 
-            if (true)
+            if (hitThisDoor)
             {
                 if (!_isLookedAt)
                 {
